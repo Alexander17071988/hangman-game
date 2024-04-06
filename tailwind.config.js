9585b0fda8ad3bccc869e733166cdff1da302c4f
Alexander Zhmurkov <a.zhmurkov@mail.ru>
2024-04-06 20:06:03 +0300

    Add hangman game files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+// export default {
+//   darkMode: "class",
+//   content: ["./index.html", "./src**/*.js"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
